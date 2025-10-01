3948cb84064feea253439d57f836c5f982fc36c6
Bryan Crompton <bryan.crompton@gmail.com>
2025-10-01 16:15:03 -0400

    remove gocode for now

--- .bashrc.d/30-navigation.sh
+++ .bashrc.d/30-navigation.sh
@@ -30,12 +30,12 @@ open () {
 # quickly open up a project in vs-code
 # vs-code saves the workspace for a given folder that is open
 # if multiple options come up, you can pick a number in the find list
-gocode () {
-    # requires fd: sudo apt install fdclone
-    proj=~/tidy/projects
-    if [ -z "$1" ]; then echo "must specify path"; return; fi;
-    if [ -z "$2" ]; then val=1; else val=$2; fi;
-    location=$(fd -t d -g "$1" $proj | head -$val | tail -1);
-    fd -t d -g "$1" $proj | head -$val | tail -1
-    code $location;
-}
+# gocode () {
+#     # requires fd: sudo apt install fdclone
+#     proj=~/tidy/projects
+#     if [ -z "$1" ]; then echo "must specify path"; return; fi;
+#     if [ -z "$2" ]; then val=1; else val=$2; fi;
+#     location=$(fd -t d -g "$1" $proj | head -$val | tail -1);
+#     fd -t d -g "$1" $proj | head -$val | tail -1
+#     code $location; # should be launched in background somehow
+# }
